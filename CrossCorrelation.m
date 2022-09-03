535ad506da5cc39e25b4c1dd5d79060183bad6b1
Mhamuda <mhamudarj05@gmail.com>
2022-09-03 20:05:19 +0600

    Cross-correlation done :)

---
+++ CrossCorrelation.m
@@ -0,0 +1,58 @@
+%Cross Correlation
+
+clc;
+clear all;
+close all;
+
+x = [0 1 2 3];
+y = [1 1 2 1];
+
+h = [];
+
+for i = length(y):-1:1
+    h  = [h y(i)];
+end
+
+arr = [];
+
+for i =1:length(x)
+    temp = h.*x(i);
+    arr = [arr;temp];
+end
+
+[row, col] = size(arr);
+sumRC = row+col;
+sum = 0;
+k=2;
+ans = [];
+
+while(k<=sumRC)
+    for i =1:row
+        for j = 1:col
+            if(i+j==k)
+                sum=sum+arr(i,j);
+            end
+        end
+    end
+    ans = [ans sum];
+    sum=0;
+    k=k+1;
+end
+
+disp(ans);
+
+subplot(3,1,1);
+stem(x);
+xlabel('x');
+ylabel('Input sequence');
+
+subplot(3,1,2);
+stem(h);
+xlabel('h');
+ylabel('Impulse sequence');
+
+subplot(3,1,3);
+stem(ans);
+xlabel('Ans');
+ylabel('Output sequence');
+title('Cross Correlation');
